4a1b364833e92fead72d067d7cc939d8610b796a
noodlechoi <choime321@gmail.com>
2025-09-12 21:29:48 +0900

    N-queen n 입력 후 board 출력 구현

---
+++ N-queen.py
@@ -0,0 +1,87 @@
+import copy, sys, queue
+
+class State:
+    def __init__(self, board, size, score=0, depth=0):
+        self.board = board
+        self.depth = depth
+        self.score = score
+        self.size = size    # n
+
+    def get_new_board(self, i, depth):
+        new_board = copy.deepcopy(self.board[:])
+        new_board[i] = 1
+        return State(new_board, self.score, self.size, depth)
+
+    def expand(self, depth):
+        result = []
+        if depth > 5: return result  # 깊이가 5 이상이면 더 이상 확장을 하지 않는다.
+        col_size = (self.size * (depth - 1))
+        for i in range(self.size):
+            result.append(self.get_new_board((i + col_size), depth))
+
+        return result
+
+    def f(self):
+        return self.h() + self.g()
+
+    # 배치했을 때 충돌하는 개수
+    def h(self):
+        score = 1
+        return score
+
+    def g(self):
+        return self.depth
+
+    def __str__(self):
+        text = f"f(n)={self.f()} h(n)={self.h()} g(n)={self.g()}\n"
+        print(self.size)
+        for i in range(self.size):
+            row = i * self.size
+            text += str(self.board[row:row+self.size]) + "\n"
+        "-----------------"
+        return text
+
+    def __eq__(self, other):
+        return self.board == other.board
+
+    def __ne__(self, other):
+        return self.board != other.board
+
+    def __lt__(self, other):
+        return self.f() < other.f()
+
+    def __gt__(self, other):
+        return self.f() > other.f()
+
+def Astar(state):
+    open_queue = queue.PriorityQueue()
+    open_queue.put(state)
+
+    closed_queue = []
+    depth = 0
+    count = 0
+    while not open_queue.empty():
+        current = open_queue.get()
+        count += 1
+        print(count)
+        print(current)
+        if current.score == 0 and depth >= current.size:
+            print("탐색 성공")
+            break
+
+        depth = current.depth + 1
+        for state in current.expand(depth):
+            if state not in closed_queue and state not in open_queue.queue:
+                open_queue.put(state)
+        closed_queue.append(current)
+    else:
+        print("탐색 실패")
+
+n = input("queen 개수 입력: ")
+n = int(n)
+puzzle = []
+for i in range(n*n):
+    puzzle.append(0)    # 0이면 빈칸
+
+Astar(State(puzzle, n))
+
